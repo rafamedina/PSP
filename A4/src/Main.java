fb37c85aba628c5cca4037549fb807c344bc2403
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-09-26 14:43:01 +0200

    tODO EN ORDEN

--- A4/src/Main.java
+++ A4/src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         try {
-
+            System.out.println("Working directory: " + System.getProperty("user.dir"));
             ProcessBuilder proceso = new ProcessBuilder("java", "-cp", "out/production/A4", "proceso");
+            proceso.directory(new File(System.getProperty("user.dir"))); // Usa el directorio actual
             Process process = proceso.start();
+
 
 
         } catch (Exception e) {
--- A4/src/metodos.java
+++ A4/src/metodos.java
@@ -6,16 +6,18 @@ public class metodos {
 
     public void tabla()  {
         try{
+            System.out.println("Working adadad: " + System.getProperty("user.dir"));
             BufferedReader input  = new BufferedReader(new FileReader("input.txt"));
             this.numero = Integer.parseInt(input.readLine());
             input.close();
+
             BufferedWriter output = new BufferedWriter(new FileWriter("output.txt"));
             for (int i = 1; i <= 10; i++) {
                 output.write(this.numero + " * " + i + " = " + this.numero * i + "\n");
             }
             output.close();
         } catch (Exception e){
-            e.getMessage();
+            e.printStackTrace();
         }
 
     }
