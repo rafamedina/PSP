2ff2db987b11e755a69b250ab5af64d7bfb87310
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-10-02 23:25:43 +0200

    A6 acabado

---
+++ A6/src/Lanzador.java
@@ -0,0 +1,13 @@
+public class Lanzador
+{
+
+
+    public static void main(String[] args) {
+        Triangulo triangulo1 = new Triangulo();
+        Triangulo triangulo2 = new Triangulo();
+        Triangulo triangulo3 = new Triangulo();
+        triangulo1.salida(5);
+        triangulo2.salida(7);
+        triangulo3.salida(9);
+    }
+}
--- A6/src/Main.java
+++ A6/src/Main.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        Triangulo triangulo = new Triangulo();
-        triangulo.salida(3);
+    public static void main(String[] args) throws IOException {
+    ProcessBuilder processs = new ProcessBuilder("java", "-cp","out/production/A6","Lanzador");
+    Process proceso = processs.start();
+
+
 
     }
 }
