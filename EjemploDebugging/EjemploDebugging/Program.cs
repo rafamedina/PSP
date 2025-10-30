20b25ef789ecb0d068ccd2ecb8764524c31c8012
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-10-30 14:04:12 +0100

    Add EjemploDebugging project files
    Initial commit for the EjemploDebugging C# project, including solution, project, source, configuration, and build output files. Also includes updates to Tik Tak Toe project metadata and IDE state files.

---
+++ EjemploDebugging/EjemploDebugging/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploDebugging
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola Buenas tardes");
+
+           
+
+
+            string patata = Console.ReadLine();
+
+            int valor = Convert.ToInt32(patata);
+
+            for (int i = 0; i < valor; i++)
+            {
+                Console.WriteLine("Valor de i: " + valor);
+            }
+        }
+    }
+}
