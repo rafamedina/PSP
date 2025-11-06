fe636a29e72f0ae3c24a74e6786bb20edb9564d6
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-11-06 13:44:56 +0100

    ASPNET 06/11/25

--- EjemploDebugging/EjemploDebugging/Program.cs
+++ EjemploDebugging/EjemploDebugging/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +11,98 @@ namespace EjemploDebugging
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hola Buenas tardes");
-
-           
-
-
-            string patata = Console.ReadLine();
-
-            int valor = Convert.ToInt32(patata);
-
-            for (int i = 0; i < valor; i++)
+            ProcesosGuapardos p = new ProcesosGuapardos();
+            // p.ejemplosGuapardos();
+            int contador = 0;
+            if (args.Length < 2)
             {
-                Console.WriteLine("Valor de i: " + valor);
+                Console.WriteLine("Uso: Subproceso_SumaParcial <inicio> <fin>");
+                return;
             }
+
+            int inicio = int.Parse(args[0]);
+            int fin = int.Parse(args[1]);
+            long suma = 0;
+
+            for (int i = inicio; i <= fin; i++)
+            {
+                if(p.esPrimo(i))
+                {
+                    contador ++;
+                }
+            
+            }
+
+            Console.WriteLine(contador);
+
+        }
+    }
+    class ProcesosGuapardos
+    {
+        public bool esPrimo(int n)
+        {
+            if (n <= 1) return false;        // 0 y 1 no son primos
+
+            for (int i = 2; i * i <= n; i++)
+            {   // Basta llegar hasta raíz cuadrada
+                if (n % i == 0)
+                {
+                    return false;             // Tiene divisor → NO es primo
+                }
+            }
+            return true;                      // No tuvo divisores → SI es primo
+        }
+        public void ejemplosGuapardos()
+        {
+            Console.WriteLine("\nHola, que programa quieres abrir?");
+            //Process paint = Process.Start("mspaint.exe");
+            bool input = false;
+            while (!input)
+            {
+                Console.WriteLine("\n1. Calculadora, \n2. Explorador carpetas \n3. Paint \n Enter para salir");
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.D1 || key.Key == ConsoleKey.NumPad1)
+                {
+                    Console.WriteLine("\nAbriendo Calculadora...");
+                    Process paint = Process.Start("calc.exe");
+                    paint.WaitForExit();
+                    while (Console.KeyAvailable) //Limpiar el Buffer
+                    {
+                        Console.ReadKey(true);
+                    }
+                    Console.WriteLine("\nCerrando Calculadora");
+                }
+                else if (key.Key == ConsoleKey.D2 || key.Key == ConsoleKey.NumPad2)
+                {
+                    Console.WriteLine("\nAbriendo Explorador...");
+                    Process paint = Process.Start("calc.exe");
+                    paint.WaitForExit();
+                    while (Console.KeyAvailable) //Limpiar el Buffer
+                    {
+                        Console.ReadKey(true);
+                    }
+                    Console.WriteLine("\nCerrando Explorador");
+                }
+                else if (key.Key == ConsoleKey.D3 || key.Key == ConsoleKey.NumPad3)
+                {
+                    Console.WriteLine("\nAbriendo Paint...");
+                    Process paint = Process.Start("mspaint.exe");
+                    paint.WaitForExit();
+                    while (Console.KeyAvailable) //Limpiar el Buffer
+                    {
+                        Console.ReadKey(true);
+                    }
+                    Console.WriteLine("\nCerrando Paint");
+
+                }
+                else if (key.Key == ConsoleKey.Enter)
+                {
+                    input = true; break;
+                }
+                else { Console.WriteLine("No valido"); }
+            }
+
+            
         }
     }
 }
---
+++ EjemploDebugging/Padre/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padre
+{ //  coger franja del subproceso y sacar os primos de esa franja y devolver cuantos numeros primos ha habido
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Suma multiproceso ===");
+
+            int[,] rangos = new int[4, 2] { { 1, 1000 }, { 1001, 2000 }, { 2001, 3000 }, { 3001, 4000 } };
+            long sumaTotal = 0;
+            bool primerparcial = false;
+            for (int i = 0; i < rangos.GetLength(0); i++)
+            {
+                int inicio = rangos[i, 0];
+                int fin = rangos[i, 1];
+
+                Process proceso = new Process();
+                //"C:\Users\CAMPUSFP\source\repos\EjemploDebugging\EjemploDebugging\bin\Debug\EjemploDebugging.exe"
+                
+                //C:\Users\CAMPUSFP\source\repos\EjemploDebugging\EjemploDebugging\bin\Debug\EjemploDebugging.exe
+                proceso.StartInfo.FileName = @"C:\Repositorio\PSP\EjemploDebugging\EjemploDebugging\bin\Debug\EjemploDebugging.exe";
+                proceso.StartInfo.Arguments = inicio + " " + fin;
+                proceso.StartInfo.RedirectStandardOutput = true;
+                proceso.StartInfo.UseShellExecute = false;
+                proceso.Start();
+
+                string salida = proceso.StandardOutput.ReadToEnd();
+                proceso.WaitForExit();
+               
+                long parcial = long.Parse(salida);
+                Console.WriteLine("Rango " + inicio + "-" + fin + " = " + parcial);
+
+               
+                    sumaTotal += parcial;
+                
+            }
+
+            Console.WriteLine("\nSuma total = " + sumaTotal);
+        }
+    }
+}
