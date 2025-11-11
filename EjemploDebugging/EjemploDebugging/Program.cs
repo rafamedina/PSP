0442bf31f638106c03853d439a27645199aad725
Rafael Medina Ayuso <rafael.medina.ayuso@gmail.com>
2025-11-11 11:27:55 +0100

    clase hilos

--- EjemploDebugging/EjemploDebugging/Program.cs
+++ EjemploDebugging/EjemploDebugging/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace EjemploDebugging
@@ -12,28 +13,45 @@ namespace EjemploDebugging
         static void Main(string[] args)
         {
             ProcesosGuapardos p = new ProcesosGuapardos();
+            HilosAunMasGuapos hm = new HilosAunMasGuapos();
+            contadorHilos ch = new contadorHilos();
+            Thread hilo1 = new Thread(new ParameterizedThreadStart(hm.EjemploHilos1));
+            Thread hilo2 = new Thread(new ParameterizedThreadStart(hm.EjemploHilos1));
+            Thread hilo3 = new Thread(new ParameterizedThreadStart(hm.EjemploHilos1));
+
+            hilo1.Start('a');
+            hilo2.Start('b');
+            hilo3.Start('c');
+
+/////////////////////////////////
+            //Thread hc = new Thread(ch.hilosecundario);
+
+            //hc.Start();
+            //ch.hiloprincipal();
+
+/////////////////////////////////
             // p.ejemplosGuapardos();
-            int contador = 0;
-            if (args.Length < 2)
-            {
-                Console.WriteLine("Uso: Subproceso_SumaParcial <inicio> <fin>");
-                return;
-            }
+            //int contador = 0;
+            //if (args.Length < 2)
+            //{
+            //    Console.WriteLine("Uso: Subproceso_SumaParcial <inicio> <fin>");
+            //    return;
+            //}
 
-            int inicio = int.Parse(args[0]);
-            int fin = int.Parse(args[1]);
-            long suma = 0;
+            //int inicio = int.Parse(args[0]);
+            //int fin = int.Parse(args[1]);
+            //long suma = 0;
 
-            for (int i = inicio; i <= fin; i++)
-            {
-                if(p.esPrimo(i))
-                {
-                    contador ++;
-                }
+            //for (int i = inicio; i <= fin; i++)
+            //{
+            //    if(p.esPrimo(i))
+            //    {
+            //        contador ++;
+            //    }
             
-            }
+            //}
 
-            Console.WriteLine(contador);
+            //Console.WriteLine(contador);
 
         }
     }
@@ -103,6 +121,106 @@ namespace EjemploDebugging
             }
 
             
+        }
+      
+
+    }
+    class HilosAunMasGuapos
+    {
+        public void EjemploHilos()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Hilo secundario : i = " + i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Fin del hilo secundario");
+        }
+
+        public void EjemploHilos1(object valor)
+        {
+            char id = (char)valor;
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"Hilo {id} => iteracion " + i);
+                Thread.Sleep(200);
+            }
+           
+        }
+
+        public void EjemploHilos2(object valor)
+        {
+            char id = (char)valor;
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"Hilo {id} => iteracion " + i);
+                Thread.Sleep(200);
+                
+            }
+
+        }
+        public void EjemploTrabajoHiloPrincipalConEjHilo2()
+        {
+            Stopwatch cronoParalelo = new Stopwatch();
+            cronoParalelo.Start();
+
+            Thread hiloA = new Thread(() => EjemploHilos2("Hilo A", 1000));
+            Thread hiloB = new Thread(() => EjemploHilos2("Hilo B", 1000));
+
+            hiloA.Start();
+            hiloB.Start();
+
+            hiloA.Join();
+            hiloB.Join();
+
+            cronoParalelo.Stop();
+
+            Console.WriteLine("Tiempo total (paralelo) :  " + cronoParalelo.ElapsedMilliseconds + " ms");
+
+            Stopwatch cronoEnSecuencial = new Stopwatch();
+
+            cronoEnSecuencial.Start();
+
+            EjemploHilos2("Secuencial A", 1000);
+            EjemploHilos2("Secuencial B", 1000);
+
+            cronoEnSecuencial.Stop();
+            Console.WriteLine("Tiempo total (secuencial) :  " + cronoEnSecuencial.ElapsedMilliseconds + " ms");
+        }
+
+
+        public void EjemploTrabajoHiloPrincipal()
+        {
+            for(int i = 0;i <= 5; i++)
+            {
+                Console.WriteLine("Hilo principal : i = " + i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Fin del hilo principal");
+        }
+    }
+
+    class contadorHilos
+    {
+        public void hiloprincipal()
+        {
+            for (int i = 1; i <= 6; i++)
+            {
+                Thread.Sleep(10000);
+                Console.WriteLine("Continuo trabajando");
+                
+            }
+            Console.WriteLine("Fin del hilo secundario");
+        }
+
+        public void hilosecundario()
+        {
+            for (int i = 1; i <= 60; i++)
+            {
+                Console.WriteLine("Contador : i = " + i);
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Fin del contador");
         }
     }
 }
